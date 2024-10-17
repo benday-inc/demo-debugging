ed311767dd1d4a19ef2ace3b9f273afe6bee19ad
Benjamin Day <benday@benday.com>
2024-10-17 11:03:50 -0400

    Add project files.

---
+++ src/Benday.DebuggingDemo.Api/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Benday.DebuggingDemo.Api;
+
+public class Class1
+{
+
+}
---
+++ test/Benday.DebuggingDemo.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Benday.DebuggingDemo.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
